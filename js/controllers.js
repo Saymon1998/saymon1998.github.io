7330044f0985edb8eabe728e0f50cd3ca8d39924
Saymon1998 <szymon.doerfer@wp.pl>
2020-05-13 16:27:54 +0200

    UPDEJT

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,18 @@
-var portfolioApp = angular.module('portfolioApp',[]);
-    portfolioApp.controller('GalleryListCtrl',function($scope, $http)
+var portfolioApp = angular.module("portfolioApp",[]);
+    portfolioApp.controller("GalleryListCtrl",function($scope, $http)
       {
-              $http.get("../images/galleries.json").then(function(response)
+              $http.get("images/galleries.json").then(function(response)
               {
                   $scope.galleries=response.data.galleries;
               }
           );
-          $scope.naglowek = 'Planety układu słonecznego!';
+          $scope.naglowek = "Planety układu słonecznego!";
 
-          $http.get("../images/sortowanie.json").then(function(response)
+          $http.get("images/sortowanie.json").then(function(response)
             {
                   $scope.sortList=response.data.sortList;
-                  $scope.orderProp = $scope.sortList[i];
-                  $scope.orderProp = 'distance';
+                  $scope.orderProp = $scope.sortList[0];
+                  $scope.orderProp = "distance";
             }
           );
-
       });
